8b703a531f8a0896ee930866009c564e8b874154
SAYEDA TANJILA AHMED (APURBA) <24-56599-1@student.aiub.edu>
2025-04-26 01:04:31 +0600

    Code to improve output tracing of increment and decrement operations.
    This code is designed to practice output tracing for increment and decrement operations, making it easier to understand exactly when and where values change during execution.

---
+++ IncrementDecrementOUTPUTpractice.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int A,B,C,D,F;
+    A=65;
+    B=A;
+    C=D++;
+    F=93;
+
+    cout<<++A<<endl;
+    cout<<A<<endl;
+    cout<<B++<<endl;
+    cout<<B<<endl;
+    cout<<C<<endl;
+    cout<<F++<<endl;
+    cout<<++F<<endl;
+    cout<<F<<endl;
+    cout<<A<<endl;
+    cout<<B<<endl;
+    cout<<C<<endl;
+    cout<<D<<endl;
+    cout<<F<<endl;
+return 0;
+}
+
