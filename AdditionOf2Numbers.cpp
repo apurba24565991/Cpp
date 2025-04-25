ff9de5a36cab9abcede279bb883f084a0009842c
SAYEDA TANJILA AHMED (APURBA) <24-56599-1@student.aiub.edu>
2025-04-25 21:29:07 +0600

    Addition of two numbers
    This C++ code helps us to add two numbers.

---
+++ AdditionOf2Numbers.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+float a,b ,addition;
+    cout<<"Enter the numbers : ";
+    cin>>a>>b;
+    addition=a+b;
+    cout<<"Addition of two numbers : "<<addition<<endl;
+return 0;
+}
+
