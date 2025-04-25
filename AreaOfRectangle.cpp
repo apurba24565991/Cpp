7a3f5fd2f3d1f2af99b1956fdb8b70116f90413d
SAYEDA TANJILA AHMED (APURBA) <24-56599-1@student.aiub.edu>
2025-04-25 21:35:49 +0600

    Area of a Rectangle
    This C++ code helps us to calculate the area of a rectangle.

---
+++ AreaOfRectangle.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int l,b,area;
+    cout<<"Input Length: "<<endl;
+    cin>>l;
+    cout<<"Input Breadth: "<<endl;
+    cin>>b;
+    area=l*b;
+    cout<<"The area of rectangle is : "<<area<<endl;
+    return 0;
+}
+
