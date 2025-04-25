92935bae0748236def5a45fbf6f94567a20109e6
SAYEDA TANJILA AHMED (APURBA) <24-56599-1@student.aiub.edu>
2025-04-26 00:53:10 +0600

    Print Name 10 times using for loop
    With the help of this C++ code, we can print any name 10 times. Here I tried to present the basic syntax of For Loop. So that the basic structure of For loop gets clear.

---
+++ ForLoopExampleNamePrinting.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    string name;
+    cout<<"Enter you name: "<<endl;
+    getline(cin,name);
+    for(int i=1;i<=10;i++)
+    {
+        cout<<name<<endl;
+    }
+return 0;
+
+}
+
