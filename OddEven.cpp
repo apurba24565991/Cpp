aa983b8fdb1e1f2a22e0e6aadb23c9b7d6b4cb87
SAYEDA TANJILA AHMED (APURBA) <24-56599-1@student.aiub.edu>
2025-04-25 20:59:24 +0600

    Whether a number is Odd or Even
    This C++ code helps us to identify whether a number is Even or Odd.

---
+++ OddEven.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int number;
+    cout<<"Enter the number"<<endl;
+    cin>>number;
+    if(number%2==0)
+     {
+         cout<<"The number is Even"<<endl;
+     }
+    else
+        {
+            cout<<"The number is odd"<<endl;
+        }
+
+return 0;
+}
+
