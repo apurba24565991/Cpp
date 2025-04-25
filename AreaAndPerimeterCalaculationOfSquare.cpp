9b32dd3a10c449f215275ab1d61ca5b68822f7e3
SAYEDA TANJILA AHMED (APURBA) <24-56599-1@student.aiub.edu>
2025-04-26 01:38:40 +0600

    Calculation of Area and Perimeter of Square.
    This C++ code helps us to determine the area and perimeter of a square.

---
+++ AreaAndPerimeterCalaculationOfSquare.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    float l,area,perimeter;
+    cout<<"Enter the Length :"<<endl;
+    cin>>l;
+    area=l*l;
+    perimeter=4*l;
+    cout<<"Area of square : "<<area<<endl;
+    cout<<"Perimeter of square : "<<perimeter<<endl;
+return 0;
+}
+
