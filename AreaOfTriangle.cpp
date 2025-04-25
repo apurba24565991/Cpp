d08f9de0468f09427cfcc335f172851d72011e31
SAYEDA TANJILA AHMED (APURBA) <24-56599-1@student.aiub.edu>
2025-04-25 21:40:13 +0600

    Area of a Triangle
    This C++ code is programmed to calculate the area of a triangle.

---
+++ AreaOfTriangle.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    float b,h,area;
+    cout<<"Input base and height : "<<endl;
+    cin>>b>>h;
+    area=0.5*(b*h);
+    cout<<"The area of triangle is : "<<area<<endl;
+return 0;
+}
+
