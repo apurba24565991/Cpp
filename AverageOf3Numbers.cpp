d2d62e3398dfd653f05a8a34e5968d69858c4892
SAYEDA TANJILA AHMED (APURBA) <24-56599-1@student.aiub.edu>
2025-04-25 21:23:39 +0600

    Average of three numbers
    This C++ code determines the Average value of three(3) numbers.

---
+++ AverageOf3Numbers.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    float a,b,c,average;
+    cout<<"Enter the three numbers :"<<endl;
+    cin>>a>>b>>c;
+    average=(a+b+c)/3;
+    cout<<"Average of three numbers is : "<<average<<endl;
+return 0;
+}
+
