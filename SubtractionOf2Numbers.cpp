bd09f4266bbc5a0abf6dec770494f4ac110cfdaa
SAYEDA TANJILA AHMED (APURBA) <24-56599-1@student.aiub.edu>
2025-04-25 21:26:41 +0600

    Subtraction of two numbers
    This C++ code helps us to determine the subtraction value of two numbers.

---
+++ SubtractionOf2Numbers.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+float a,b ,subtraction;
+    cout<<"Enter the numbers : ";
+    cin>>a>>b;
+    subtraction=a-b;
+    cout<<"Subtraction of two numbers : "<<subtraction<<endl;
+return 0;
+}
+
