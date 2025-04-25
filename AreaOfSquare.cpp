580e6b072ffddf6ea8d1ca54b351df61f13d04c8
SAYEDA TANJILA AHMED (APURBA) <24-56599-1@student.aiub.edu>
2025-04-25 21:32:15 +0600

    Area of a Square
    This C++ code helps us to calculate the area of a square.

---
+++ AreaOfSquare.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    float l,area;
+    cout<<"Input Length : "<<endl;
+    cin>>l;
+    area=l*l;
+    cout<<"The area of square is : "<<area<<endl;
+return 0;
+}
+
