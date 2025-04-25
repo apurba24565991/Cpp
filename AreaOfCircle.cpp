d5f58da8a0a5fa2e0b319a7fddf06abad6f83e28
SAYEDA TANJILA AHMED (APURBA) <24-56599-1@student.aiub.edu>
2025-04-25 21:19:37 +0600

    Area of a Circle
    This C++ code is used to determine the area of Circle.

---
+++ AreaOfCircle.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    float r,area;
+    cout<<"Enter the radius :"<<endl;
+    cin>>r;
+    area=3.1416*r*r;
+    cout<<"Area of circle is :"<<area<<endl;
+return 0;
+}
+
